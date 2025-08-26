5996352228292d9a4c0e8b4a5d81d8b02ff54a29
Aaron Chien <102495640+achien2024@users.noreply.github.com>
2025-08-25 21:30:01 -0700

    Add files via upload

---
+++ projects/stock_tracker/portfoliowatch.py
@@ -0,0 +1,11 @@
+# dictionary
+# dictionary of ticker: price
+# make OOP
+# .add(): adds to orginal price (you are investing more)
+# .price(): gets today's price, output the amount you invested in
+# .total(): gets today's price for all stocks, output amount you invested in
+# .summary(): gets percentage changes 
+
+# VTI: $296.13
+# QQQ: $532.41
+
